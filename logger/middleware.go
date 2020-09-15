c8ab3fb1b4a85fda99dcc32332b77ac6ef2465de
AnuchitO <anuchit.prasertasng@gmail.com>
2020-09-15 16:54:34 +0700

    7.5.1 Logger : still work key is still string

--- logger/middleware.go
+++ logger/middleware.go
@@ -6,16 +6,18 @@ import (
 	"net/http"
 )
 
+const key = "logger"
+
 func LoggerMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		l, _ := zap.NewDevelopment()
 		l = l.With(zap.Namespace("hometic"), zap.String("I'm", "gopher"))
-		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), "logger", l)))
+		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), key, l)))
 	})
 }
 
 func L(ctx context.Context) *zap.Logger {
-	v := ctx.Value("logger")
+	v := ctx.Value(key)
 	if v == nil{
 		return zap.NewExample()
 	}
